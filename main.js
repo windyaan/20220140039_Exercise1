53f398d18a4647d8f35711aed685687a5503c6dd
windyaan <windyanurmaretha@gmail.com>
2024-03-24 15:06:23 +0700

    first

---
+++ main.js
@@ -0,0 +1,50 @@
+function show1(paragraphId){
+    document.getElementById('gambar').style.display = 'none';
+    document.getElementById('gambar1').style.display = 'block';
+    document.getElementById('gambar2').style.display = 'none';
+    document.getElementById('gambar3').style.display = 'none';
+    document.getElementById('gambar4').style.display = 'none';
+    document.getElementById('paragraf3').style.display = 'none';
+    document.getElementById('paragraf4').style.display = 'none';
+    document.getElementById('paragraf1').style.display = 'block';
+    document.getElementById('paragraf2').style.display = 'none';
+}
+
+function show2(paragraphId){
+    document.getElementById('gambar').style.display = 'none';
+    document.getElementById('gambar1').style.display = 'none';
+    document.getElementById('gambar2').style.display = 'block';
+    document.getElementById('gambar3').style.display = 'none';
+    document.getElementById('gambar4').style.display = 'none';
+    document.getElementById('paragraf1').style.display = 'none';
+    document.getElementById('paragraf2').style.display = 'block';
+    document.getElementById('paragraf3').style.display = 'none';
+    document.getElementById('paragraf4').style.display = 'none';
+}
+
+function show3(paragraphId){
+    document.getElementById('gambar').style.display = 'none';
+    document.getElementById('gambar1').style.display = 'none';
+    document.getElementById('gambar2').style.display = 'none';
+    document.getElementById('gambar3').style.display = 'block';
+    document.getElementById('gambar4').style.display = 'none';
+    document.getElementById('paragraf1').style.display = 'none';
+    document.getElementById('paragraf2').style.display = 'none';
+    document.getElementById('paragraf3').style.display = 'block';
+    document.getElementById('paragraf4').style.display = 'none';
+}
+
+function show4(paragraphId){
+    document.getElementById('gambar').style.display = 'none';
+    document.getElementById('gambar1').style.display = 'none';
+    document.getElementById('gambar2').style.display = 'none';
+    document.getElementById('gambar3').style.display = 'none';
+    document.getElementById('gambar4').style.display = 'block';
+    document.getElementById('paragraf1').style.display = 'none';
+    document.getElementById('paragraf2').style.display = 'none';
+    document.getElementById('paragraf3').style.display = 'none';
+    document.getElementById('paragraf4').style.display = 'block';
+}
+function show5(){
+    alert("Tiket Telah Terpesan")
+}
